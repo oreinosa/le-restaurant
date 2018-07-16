3acb55548e45b0f34146366f5e58e08a8ea9397f
Oscar Reinosa <oscar.reinosa96@gmail.com>
2018-07-16 18:00:33 -0500

    update

--- angular-front-end/src/app/admin/users/list/list.component.ts
+++ angular-front-end/src/app/admin/users/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 import { User } from '../../../shared/classes/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -11,11 +12,23 @@ export class ListComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'email', 'role', 'actions'];
   users: User[];
   constructor(
-    private usersService: UsersService
+    private usersService: UsersService,
+    private router: Router
   ) { }
 
   ngOnInit() {
-    this.usersService.getUsers().subscribe((res: any) => this.users = res.success ? res.users : []);
+    this.usersService
+      .getUsers()
+      .subscribe((res: any) => this.users = res.success ? res.users : []);
+  }
+
+  onAction(action: string, user: User) {
+    let id = '';
+    if (user) {
+      id = user.id;
+      this.usersService.setSelectedUser(user);
+    }
+    this.router.navigate(['users', action, id]);
   }
 
 }
--- angular-front-end/src/app/admin/users/users.service.ts
+++ angular-front-end/src/app/admin/users/users.service.ts
@@ -1,18 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../../shared/classes/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   private api = 'http://localhost:3000/users/';
+  private selectedUserSubject = new BehaviorSubject<User>(null);
 
   constructor(
     private http: HttpClient
   ) { }
 
+  getSelectedUser(): Observable<User> {
+    return this.selectedUserSubject.asObservable();
+  }
+
+  setSelectedUser(user: User): void {
+    return this.selectedUserSubject.next(user);
+  }
+
+  isUserSelected(): boolean {
+    return !!this.selectedUserSubject.getValue();
+  }
+
   getUsers() {
     return this.http.get(this.api);
+  }
+
+  getUser(id: string) {
+    return this.http.get(this.api + id);
   }
 
   deleteUser(id: string) {
