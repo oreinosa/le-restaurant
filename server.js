cbda90654adaea2e578b6503eae503c4fcd574a1
Oscar Reinosa <oscar.reinosa96@gmail.com>
2018-07-25 15:23:30 -0600

    update

--- server.js
+++ server.js
@@ -10,55 +10,45 @@ var mongoose = require("mongoose");
 var logger = require("morgan");
 var user_router_1 = require("./router/user.router");
 var userRouter = new user_router_1.UserRouter();
-var Server = (function() {
-  function Server() {
-    this.app = express();
-    this.config();
-    this.routes();
-  }
-  Server.prototype.config = function() {
-    var MONGO_URI =
-      "mongodb://dbuser:code123@ds249311.mlab.com:49311/restaurant?authSource=restaurant&w=1";
-    mongoose
-      .connect(
-        MONGO_URI,
-        {
-          useNewUrlParser: true
-        }
-      )
-      .then(function() {
-        return console.log("Connected succesfully to DB " + MONGO_URI);
-      })
-      .catch(function(err) {
-        return console.log("ERROR ", err);
-      });
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-    this.app.use(bodyParser.json());
-    this.app.use(cookieParser());
-    this.app.use(logger("dev"));
-    this.app.use(compression());
-    this.app.use(helmet());
-    this.app.use(cors());
-    this.app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-      res.header(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-      );
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
-      );
-      res.header("Access-Control-Allow-Credentials", "true");
-      next();
-    });
-  };
-  Server.prototype.routes = function() {
-    var router = express.Router();
-    this.app.use("/", router);
-    this.app.use("/api/v1/users", userRouter.router);
-  };
-  return Server;
-})();
+var Server = (function () {
+    function Server() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+    Server.prototype.config = function () {
+        var MONGO_URI = "mongodb://ds249311.mlab.com:49311/restaurant";
+        mongoose
+            .connect(MONGO_URI || process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            auth: {
+                user: "dbuser",
+                password: "code123"
+            }
+        })
+            .then(function () { return console.log("Connected succesfully to DB " + MONGO_URI); })
+            .catch(function (err) { return console.log("ERROR ", err); });
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(cookieParser());
+        this.app.use(logger("dev"));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
+        this.app.use(function (req, res, next) {
+            res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
+            res.header("Access-Control-Allow-Credentials", "true");
+            next();
+        });
+    };
+    Server.prototype.routes = function () {
+        var router = express.Router();
+        this.app.use("/", router);
+        this.app.use("/api/v1/users", userRouter.router);
+    };
+    return Server;
+}());
 exports.default = new Server().app;
 //# sourceMappingURL=server.js.map
