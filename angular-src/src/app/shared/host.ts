5bd2f6320d4e920e163530f542e009d433fac6f2
oreinosa <31995668+oreinosa@users.noreply.github.com>
2018-07-29 20:01:13 -0600

    update

--- angular-src/src/app/shared/host.ts
+++ angular-src/src/app/shared/host.ts
@@ -1,3 +1,2 @@
-import { environment } from "../../environments/environment";
 
-export const HOST = environment.production ? 'http://localhost:3000/api/v1/' : 'http://localhost:8080/api/v1/';
+export const HOST = 'http://localhost:8080/api/v1/';
