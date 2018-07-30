54dcb671ad9b02ecc88a24bc18b0de6109fce598
oreinosa <31995668+oreinosa@users.noreply.github.com>
2018-07-29 19:48:50 -0600

    update

--- angular-src/src/app/shared/host.ts
+++ angular-src/src/app/shared/host.ts
@@ -1,1 +1,3 @@
-export const HOST = 'http://localhost:3000/api/v1/';
+import { isDevMode } from '@angular/core';
+
+export const HOST = isDevMode() ? 'http://localhost:3000/api/v1/' : 'http://localhost:8080/api/v1/';
