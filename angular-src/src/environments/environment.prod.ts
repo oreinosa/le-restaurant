0bff36753b2efc962c7ada6218fa0136abe8518c
oreinosa <31995668+oreinosa@users.noreply.github.com>
2018-07-31 19:39:56 -0600

    update

--- angular-src/src/environments/environment.prod.ts
+++ angular-src/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://127.0.0.1/api/v1/"
+  // api: "http://127.0.0.1/api/v1/"
 
-  // api: "https://thenewfuturesv.com/api/v1/"
+  api: "https://thenewfuturesv.com/api/v1/"
 };
